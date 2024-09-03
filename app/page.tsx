c1b6430c87fd66957e953f0bff71486d71d8480e
an0n99 <jgr92@proton.me>
2024-09-03 12:59:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,23 +29,23 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        st3rlxx
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
+          I am James, an entrepreneur and Web3 developer building{" "}
           <Link
             target="_blank"
-            href="https://unkey.dev"
+            href="https://jgr929.wixsite.com/arcanium"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            Arcanium
+          </Link>
+          . A P2P solution to a highly centralized Real Estate Market.
         </h2>
       </div>
     </div>
   );
-
 }
